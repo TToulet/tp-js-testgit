aa0b1b18d492e9c0b319da46a3a8bd4dfe542e6a
leo <leo.rasoloma@ortmontreil.fr>
2024-01-29 11:15:55 +0100

    tout bien mis

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 var text = document.getElementById('app').textContent;
 alert(text)
+
+document.addEventListener("DOMContentLoaded", function() {
+    const appDiv = document.getElementById("app");
+
+    // Démarrer l'animation après 2 secondes
+    setTimeout(function() {
+        appDiv.style.animationPlayState = 'running';
+    }, 2000);
+
+    // Pour arrêter l'animation, utilisez :
+    // appDiv.style.animationPlayState = 'paused';
+});
